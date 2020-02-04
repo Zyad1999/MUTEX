3992bed4a10ef63828142085e0cfbea7b85a1136
geekahmed1@gmail.com <geekahmed1@gmail.com>
2020-02-04 16:21:30 +0200

    Internet Connection Check

--- app/src/main/java/org/ieeezsb/MainActivity.java
+++ app/src/main/java/org/ieeezsb/MainActivity.java
@@ -1,6 +1,10 @@
 package org.ieeezsb;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,8 +19,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        };
 
+
+        }
+
+        // Method to Check Internet Connection.
+    private boolean haveConnectionToInternet (){
+
+        boolean haveWifi = false ;
+        boolean haveMobileData = false;
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo[] networkInfos = cm.getAllNetworkInfo();
+        for (NetworkInfo info: networkInfos){
+            if (info.getTypeName().equalsIgnoreCase("wifi")){
+                if (info.isConnected()){
+                    haveWifi = true;
+                }
+            }
+
+            if (info.getTypeName().equalsIgnoreCase("mobile")){
+                if (info.isConnected()){
+                    haveMobileData = true;
+                }
+            }
+        }
+
+        return haveMobileData || haveWifi;
+
+        }
 
 
 }
